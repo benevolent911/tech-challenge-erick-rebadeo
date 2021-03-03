1e151a3755e5159636059f790806fe473fcbf089
erickrebadeo <erick.rebadeo@gmail.com>
2021-03-03 23:41:17 +0800

    Update app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SpeechesRoutingModule } from './speeches/speeches-routing.module';
 import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+import { ShareSpeechComponent } from './shared/share-speech/share-speech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShareSpeechComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    ToastrModule.forRoot()
+    CommonModule,
+    HttpClientModule,
+    SpeechesRoutingModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ShareSpeechComponent]
 })
 export class AppModule { }
