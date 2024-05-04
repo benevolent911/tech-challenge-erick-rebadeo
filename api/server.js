8f61179571b00cc62498b82f11a556cef8e4a771
Erick Rebadeo <erick.rebadeo@ffufm.com>
2024-05-04 23:02:15 +0800

    add api route

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const speechRoute = require("./routes/speechRoute");
+const errorMiddleware = require("./middleware/errorMiddleware");
 const cors = require("cors");
 
 const app = express();
@@ -12,6 +14,8 @@ const MONGO_URL = process.env.MONGO_URL;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/speeches", speechRoute);
 
 app.use(errorMiddleware);
 
